728d6680891fdbeaa72d70eff5a83aa9ea888928
xiaobo1997 <452387280@qq.com>
2023-08-19 20:20:42 +0800

    300 动态规划 最长递增子序列

---
+++ lc-viw/code/.leetcode/300.最长递增子序列.java
@@ -0,0 +1,33 @@
+import java.util.Arrays;
+
+/*
+ * @lc app=leetcode.cn id=300 lang=java
+ *
+ * [300] 最长递增子序列
+ */
+
+// @lc code=start
+class Solution {
+    //动态规划
+    public int lengthOfLIS(int[] nums) {
+        // [2,1,2,3]
+        if(nums.length==0) return 0;
+        int[] dp = new int[nums.length];
+        int res = 0;
+        // [1,1,1,1]
+        Arrays.fill(dp,1);
+        //遍历nums
+        for(int i = 0;i<nums.length;i++){
+            //遍历
+            for(int j = 0;j<i;j++){
+                if(nums[j]<nums[i]){
+                    dp[i] = Math.max(dp[i],dp[j]+1);
+                }
+            }
+            //维持最大的
+            res = Math.max(res,dp[i]);
+        }
+        return res;
+    }
+}
+// @lc code=end
