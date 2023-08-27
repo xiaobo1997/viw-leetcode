6f6dac60ce859f7e617882644f1afc719d96ea3e
xiaobo1997 <452387280@qq.com>
2023-08-28 02:12:05 +0800

    [101] 对称二叉树

--- lc-viw/code/.leetcode/101.对称二叉树.java
+++ lc-viw/code/.leetcode/101.对称二叉树.java
@@ -38,10 +38,13 @@ class Solution {
 
 class Solution {
     public boolean isSymmetric(TreeNode root) {
-
+        if(root==null) return false;
+        return is(root.left,root.right);
     }
 
-      private boolean isSymmetric(TreeNode left,TreeNode right){
-
+      private boolean is(TreeNode left,TreeNode right){
+        if(left==null || right ==null) return left==right;
+        if(left.val!=right.val) return false;
+        return is(left.left,right.right)&& is(left.right,right.left);
     }
 }
