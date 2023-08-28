15e028cc0b44967ad800b924a2e0b7341fcec7e1
xiaobo1997 <452387280@qq.com>
2023-08-28 18:30:17 +0800

    [19] 删除链表的倒数第 N 个结点   [142] 环形链表 II

--- lc-viw/code/.leetcode/53.最大子数组和.java
+++ lc-viw/code/.leetcode/53.最大子数组和.java
@@ -2,6 +2,10 @@
  * @lc app=leetcode.cn id=53 lang=java
  *
  * [53] 最大子数组和
+ * 
+ * 输入：nums = [-2,1,-3,4,-1,2,1,-5,4]
+输出：6
+解释：连续子数组 [4,-1,2,1] 的和最大，为 6 。
  */
 
 // @lc code=start
@@ -22,3 +26,18 @@ class Solution {
 }
 // @lc code=end
 
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int len = nums.length;
+        int sum = 0,max = Integer.MAX_VALUE;
+        for(int i = 0;i<len;i++){
+            if(sum>0){
+                sum+=nums[i];
+            }else {
+                sum = nums[i];
+            }
+            max = Math.max(sum,max);
+        }
+        return max;
+    }
+}
---
+++ lc-viw/code/.leetcode/56.合并区间.java
@@ -0,0 +1,17 @@
+/*
+ * @lc app=leetcode.cn id=56 lang=java
+ *
+ * [56] 合并区间
+ * 输入：intervals = [[1,4],[4,5]]
+输出：[[1,5]]
+解释：区间 [1,4] 和 [4,5] 可被视为重叠区间。
+ */
+
+// @lc code=start
+class Solution {
+    public int[][] merge(int[][] intervals) {
+
+    }
+}
+// @lc code=end
+
