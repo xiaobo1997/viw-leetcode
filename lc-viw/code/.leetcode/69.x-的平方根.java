bc16702de36e5b8ffa25dd0f8b88011159dda82c
xiaobo1997 <452387280@qq.com>
2023-08-29 10:31:58 +0800

    [69] x 的平方根

---
+++ lc-viw/code/.leetcode/69.x-的平方根.java
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=69 lang=java
+ *
+ * [69] x 的平方根 
+ */
+
+// @lc code=start
+class Solution {
+    public int mySqrt(int x) {
+         long r= x;
+         while(r*r>x){
+            r = (r+x/r)/2;
+         }
+         return (int)r;
+    }
+}
+// @lc code=end
+
+class Solution {
+    public int mySqrt(int x) {
+        long r = x;
+        while(r*r >x)
+            r = (r+x/r)/2;
+        return r;
+    }
+}
