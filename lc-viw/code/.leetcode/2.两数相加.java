71fc78b235199d82648f97d9040e071890df707e
xiaobo1997 <452387280@qq.com>
2023-08-29 12:00:40 +0800

    [2] 两数相加

---
+++ lc-viw/code/.leetcode/2.两数相加.java
@@ -0,0 +1,36 @@
+/*
+ * @lc app=leetcode.cn id=2 lang=java
+ *
+ * [2] 两数相加
+ */
+
+// @lc code=start
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+     public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+        return dfs(l1,l2,0);
+    }
+    ListNode dfs(ListNode l1,ListNode l2, int i){
+        if(l1==null&&l2==null&&i==0) return null;
+        int sum=(l1!=null?l1.val:0)+(l2!=null?l2.val:0)+i;
+        var node = new ListNode(sum%10);
+        node.next = dfs(l1!=null?l1.next:null,l2!=null?l2.next:null,sum/10);
+        return node;
+    }
+}
+// @lc code=end
+
+class Solution {
+    public ListNode addTwoNumbers(ListNode l1, ListNode l2) {
+    
+    }
+}
