00a8934b8cbb9948233a7093d9da3b90cf6f716d
xiaobo1997 <452387280@qq.com>
2024-11-07 23:54:18 +0800

    commit

---
+++ lc-viw/code/.leetcode/31.下一个排列.java
@@ -0,0 +1,15 @@
+/*
+ * @lc app=leetcode.cn id=31 lang=java
+ *
+ * [31] 下一个排列
+ * 力扣Hot 100
+ */
+
+// @lc code=start
+class Solution {
+    public void nextPermutation(int[] nums) {
+
+    }
+}
+// @lc code=end
+
